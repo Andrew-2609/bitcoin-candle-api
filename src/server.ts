2376051504a4fd9501254accb139e566a7ef77f1
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-11-24 21:55:25 -0300

    feat: consume messages with CandleMessageChannel on server start

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { config } from 'dotenv'
 import { connection } from 'mongoose'
 import { app } from './app'
 import { connectToMongoDb } from './config/db'
+import { CandleMessageChannel } from './messages/CandleMessageChannel'
 
 const createServer = async (): Promise<void> => {
   config()
@@ -10,6 +11,9 @@ const createServer = async (): Promise<void> => {
 
   const PORT = String(process.env.APP_PORT)
   const server = app.listen(PORT, () => console.log(`App running at ${PORT}`))
+
+  const candleMsgChannel = new CandleMessageChannel(server)
+  candleMsgChannel.consumeMessages()
 
   process.on('SIGINT', async () => {
     await connection.close()
