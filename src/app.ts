f05e9751b947755a31c21f02c1b1b55395ce84f0
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-11-24 21:58:34 -0300

    feat: use candleRouter in app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import * as cors from 'cors'
 import * as express from 'express'
 import * as logger from 'morgan'
+import { candleRouter } from './routes/candles'
 
 export const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(logger('dev'))
+
+// Routes
+app.use(`/candles`, candleRouter)
